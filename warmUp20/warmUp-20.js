d36d0b7d716ce6c54930a49e4f663e5caa263708
aliSoudani <lkollasfbt@gmail.com>
2020-02-28 09:21:03 +0100

    warmup20

--- warmUp20/warmUp-20.js
+++ warmUp20/warmUp-20.js
@@ -10,9 +10,11 @@ function reverseStr(str){
   var array=str.split(" ");
   var result=[];
   var i=array.length;
+  // we will return the array result with reversed word 
   while(i--){
     result.push(array[i])
   }
+  //  we will concatinate the result so the out put will be string
  for(var j =0; j < result.length; j++){
    string=string+" "+result[j];
  }
